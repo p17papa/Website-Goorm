e9788f33004e693099ae29b04059ee9bdcb11f06
Παναγιώτης Παπαθανασίου <44357183+p17papa@users.noreply.github.com>
2023-05-06 20:16:47 +0300

    Update home.php
    This is working with the questions and the answers. It's without any css into in

--- home.php
+++ home.php
@@ -1,9 +1,40 @@
 <html>
 <head>
-	<title>Home</title>
+    <title>Home</title>
+	<style>
+		#user-info {
+			position: absolute;
+			top: 0;
+			right: 0;
+		}
+	</style>
 </head>
 <body>
-	<h1>Welcome to the site!</h1>
+    <?php
+    // Start the session and check if the user is logged in
+    session_start();
+    if (!isset($_SESSION["user_id"])) {
+        header("Location: login.php");
+        exit();
+    }
+    // Connect to the database
+    $servername = "localhost";
+    $username = "phpmyadmin";
+    $password = "admin";
+    $dbname = "UserData";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    // Retrieve the logged-in user's name
+    $user_id = $_SESSION["user_id"];
+    $sql_user = "SELECT username FROM user WHERE id = '$user_id'";
+    $result_user = mysqli_query($conn, $sql_user);
+    $row_user = mysqli_fetch_assoc($result_user);
+    $username = $row_user["username"];
+    ?>
+    
+	
 	<h2>Ask a question:</h2>
 	<form action="post_question.php" method="post">
 		<label for="question_title">Title:</label>
@@ -13,38 +44,61 @@
 		<textarea id="question_body" name="question_body"></textarea><br><br>
 		
 		<input type="submit" value="Post Question">
+		<a href="logout.php">Logout</a>
 	</form>
-</body>
 	
-</html>
+	<h1>Recent Questions</h1>
+	<?php
+	// Connect to the database
+	$servername = "localhost";
+	$username = "phpmyadmin";
+	$password = "admin";
+	$dbname = "UserData";
 
-<?php
-//echo $_SESSION['user_id'];
-// Connect to the database
-$servername = "localhost";
-$username = "phpmyadmin";
-$password = "admin";
-$dbname = "UserData";
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	if (!$conn) {
+	    die("Connection failed: " . mysqli_connect_error());
+	}
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+	// Retrieve the questions from the database
+	//$sql = "SELECT * FROM question ORDER BY date_added DESC";
+	$sql = "SELECT question.*, user.username FROM question LEFT JOIN user ON question.user_id = user.id ORDER BY date_added DESC";
+	$result = mysqli_query($conn, $sql);
 
-// Retrieve the questions from the database
-$sql = "SELECT * FROM question ORDER BY date_added DESC";
-$result = mysqli_query($conn, $sql);
-
-// Display the questions
-echo "<h1>Recent Questions</h1>";
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<h3>" . $row["title"] . "</h3>";
-    echo "<p>By: " . $row["user_id"] . "</p>";
-    echo "<p>" . $row["question_text"] . "</p>";
+	// Display the questions
+	while ($row = mysqli_fetch_assoc($result)) {
+	    echo "<h3>" . $row["title"] . "</h3>";
+	    echo "<p>By: " . $row["username"] . "</p>";
+	    echo "<p>" . $row["content"] . "</p>";
+	    echo "<p>Submitted on: " . $row["date_added"] . "</p>";
+	    
+	    // Display the answers to the question
+	    $question_id = $row["id"];
+	    $sql_answer = "SELECT * FROM answer WHERE question_id = '$question_id' ORDER BY date_added ASC";
+	    $result_answer = mysqli_query($conn, $sql_answer);
+	    
+	    // Display the answer form
+	    echo '<form action="post_answer.php" method="post">';
+	    echo '<input type="hidden" name="question_id" value="' . $question_id . '">';
+	    echo '<label for="answer">Your answer:</label><br>';
+	    echo '<textarea id="answer_text" name="answer_text"></textarea><br><br>';
+	    echo '<input type="submit" value="Post Answer">';
+	    echo '</form>';
+	    
+	    // Display the answers to the question
+	    while ($row_answer = mysqli_fetch_assoc($result_answer)) {
+    echo "<p>Answer by " . $row["username"] . ":</p>";
+    echo "<p>" . $row_answer["answer_text"] . "</p>";
+    echo "<p>Submitted on: " . $row_answer["date_added"] . "</p>";
     echo "<hr>";
 }
 
-// Close the database connection
-mysqli_close($conn);
-?>
+	    
+	    echo "<hr>";
+	}
 
+	// Close the database connection
+	mysqli_close($conn);
+	?>
+</body>
+</html>
